867d47d6d74f8074640a49fad131d39ddfa3c8ac
Goutham Veeramachaneni <cs14btech11014@iith.ac.in>
2017-04-05 19:03:31 +0530

    POC to demo lockup
    Signed-off-by: Goutham Veeramachaneni <cs14btech11014@iith.ac.in>

--- head_test.go
+++ head_test.go
@@ -1,11 +1,16 @@
 package tsdb
 
 import (
+	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
+	"strconv"
 	"testing"
+	"time"
 	"unsafe"
 
+	"github.com/go-kit/kit/log"
 	"github.com/pkg/errors"
 	"github.com/prometheus/tsdb/labels"
 
@@ -71,4 +76,47 @@ func readPrometheusLabels(fn string, n int) ([]labels.Labels, error) {
 		return mets, errors.Errorf("requested %d metrics but found %d", n, i)
 	}
 	return mets, nil
+}
+
+func TestDeadlock(t *testing.T) {
+
+	dir, err := ioutil.TempDir("", "test_persistence_e2e")
+	require.NoError(t, err)
+	defer os.RemoveAll(dir)
+
+	lbls, err := readPrometheusLabels("testdata/20k.series", 1)
+	require.NoError(t, err)
+
+	l := log.NewLogfmtLogger(os.Stdout)
+	ts := time.Now().UnixNano() / 1000000
+
+	hb, err := createHeadBlock(dir, 0, l, ts, ts+50*1000)
+	require.NoError(t, err)
+
+	for i := 0; i < 10; i++ {
+		ts := time.Now().UnixNano() / 1000000
+		for j := 0; j < 10; j++ {
+			app := hb.Appender()
+			go ingestScrapes(t, lbls, strconv.Itoa(j), ts, app)
+		}
+		time.Sleep(1 * time.Second)
+	}
+
+	fmt.Println(hb.Meta().Stats, hb.Busy())
+}
+
+func ingestScrapes(
+	t *testing.T,
+	l []labels.Labels,
+	instance string,
+	ts int64,
+	app Appender,
+) {
+	for _, lset := range l {
+		lset = append(lset, labels.Label{Name: "instance", Value: instance})
+		_, err := app.Add(lset, ts, rand.Float64())
+		require.NoError(t, err)
+	}
+	require.NoError(t, app.Commit())
+	return
 }
