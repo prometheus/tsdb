bbb7013045a66930fe6f4a919a33c8c2e07c07a4
Goutham Veeramachaneni <cs14btech11014@iith.ac.in>
2017-09-08 00:35:28 +0530

    Compress the series chunk details in index.
    Signed-off-by: Goutham Veeramachaneni <cs14btech11014@iith.ac.in>

--- index.go
+++ index.go
@@ -291,11 +291,24 @@ func (w *indexWriter) AddSeries(ref uint64, lset labels.Labels, chunks ...ChunkM
 	}
 
 	w.buf2.putUvarint(len(chunks))
+	var (
+		t0   int64
+		ref0 uint64
+	)
 
-	for _, c := range chunks {
+	if len(chunks) > 0 {
+		c := chunks[0]
 		w.buf2.putVarint64(c.MinTime)
-		w.buf2.putVarint64(c.MaxTime)
+		w.buf2.putVarint64(c.MaxTime - c.MinTime)
 		w.buf2.putUvarint64(c.Ref)
+		t0 = c.MinTime
+		ref0 = c.Ref
+
+		for _, c := range chunks[1:] {
+			w.buf2.putVarint64(c.MinTime - t0)
+			w.buf2.putVarint64(c.MaxTime - t0)
+			w.buf2.putUvarint64(c.Ref - ref0)
+		}
 	}
 
 	w.buf1.reset()
@@ -775,10 +788,29 @@ func (r *indexReader) Series(ref uint64, lbls *labels.Labels, chks *[]ChunkMeta)
 	// Read the chunks meta data.
 	k = int(d2.uvarint())
 
-	for i := 0; i < k; i++ {
-		mint := d2.varint64()
-		maxt := d2.varint64()
-		off := d2.uvarint64()
+	var (
+		t0   int64
+		ref0 uint64
+	)
+
+	if k == 0 {
+		return nil
+	}
+
+	t0 = d2.varint64()
+	maxt := d2.varint64() + t0
+	ref0 = d2.uvarint64()
+
+	*chks = append(*chks, ChunkMeta{
+		Ref:     ref0,
+		MinTime: t0,
+		MaxTime: maxt,
+	})
+
+	for i := 1; i < k; i++ {
+		mint := d2.varint64() + t0
+		maxt := d2.varint64() + t0
+		off := d2.uvarint64() + ref0
 
 		if d2.err() != nil {
 			return errors.Wrapf(d2.err(), "read meta for chunk %d", i)
