f1ec2cce68b4d4c7969586aae55313c5551ae70e
nilsocket <nilsocket@gmail.com>
2018-10-29 11:58:00 +0530

    FromMap(), sort and return instead of calling New()
    Signed-off-by: nilsocket <nilsocket@gmail.com>

--- labels/labels.go
+++ labels/labels.go
@@ -121,7 +121,8 @@ func FromMap(m map[string]string) Labels {
 	for k, v := range m {
 		l = append(l, Label{Name: k, Value: v})
 	}
-	return New(l...)
+	sort.Sort(Labels(l))
+	return l
 }
 
 // FromStrings creates new labels from pairs of strings.
