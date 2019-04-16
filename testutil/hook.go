b97c06f650ed0c603df1928ba0951cfb37a0998f
qiffang <947321353@qq.com>
2019-04-16 16:01:58 +0800

    add-go-fuse-to-inject-filesystem-error
    Signed-off-by: qiffang <947321353@qq.com>

---
+++ testutil/hook.go
@@ -0,0 +1,10 @@
+package testutil
+
+type Hook interface{}
+
+type HookContext interface{}
+
+type HookOnRename interface {
+	PreRename(oldPatgh string, newPath string) (hooked bool, err error)
+	PostRename(oldPatgh string, newPath string) (hooked bool, err error)
+}
