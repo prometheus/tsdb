694e09a7d1f19a6cc0e4f1428eb7b835c7a22974
naivewong <867245430@qq.com>
2019-06-14 10:43:26 +0800

    optimize with genSeries
    Signed-off-by: naivewong <867245430@qq.com>

--- head_test.go
+++ head_test.go
@@ -19,7 +19,6 @@ import (
 	"math/rand"
 	"os"
 	"path"
-	"path/filepath"
 	"sort"
 	"testing"
 
@@ -34,12 +33,7 @@ import (
 )
 
 func BenchmarkCreateSeries(b *testing.B) {
-	n := b.N
-	if n > 20000 {
-		n = 20000
-	}
-	lbls, err := labels.ReadLabels(filepath.Join("testdata", "20kseries.json"), n)
-	testutil.Ok(b, err)
+	series := genSeries(b.N, 10, 0, 0)
 
 	h, err := NewHead(nil, nil, nil, 10000)
 	testutil.Ok(b, err)
@@ -48,8 +42,8 @@ func BenchmarkCreateSeries(b *testing.B) {
 	b.ReportAllocs()
 	b.ResetTimer()
 
-	for _, l := range lbls {
-		h.getOrCreate(l.Hash(), l)
+	for _, s := range series {
+		h.getOrCreate(s.Labels().Hash(), s.Labels())
 	}
 }
 
